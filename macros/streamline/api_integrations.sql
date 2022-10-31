bef444eebd00a4837666f164a3d68f176f5090a2
cantjaso <108745501+cantjaso@users.noreply.github.com>
2022-10-31 13:51:26 -0400

    Jason/blocks (#37)
    * Models for blocks stream
    
    * add the utils for post_hook
    
    * add the bronze streamline schema
    
    * Update
    
    * Update
    
    * Duplicate bug fix
    
    * Chainhead route fix
    
    * Bug fix
    
    Co-authored-by: yulike <xiuyangguan@gmail.com>

---
+++ macros/streamline/api_integrations.sql
@@ -0,0 +1,11 @@
+{% macro create_aws_polygon_api() %}
+    {% if target.name == "prod" %}
+        {% set sql %}
+        CREATE api integration IF NOT EXISTS aws_polygon_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::661245089684:role/snowflake-api-polygon' api_allowed_prefixes = (
+            'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod/',
+            'https://jml4wcap5f.execute-api.us-east-1.amazonaws.com/dev/'
+        ) enabled = TRUE;
+{% endset %}
+        {% do run_query(sql) %}
+    {% endif %}
+{% endmacro %}
