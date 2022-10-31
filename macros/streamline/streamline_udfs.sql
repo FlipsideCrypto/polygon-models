ade4059766632998f997ef79d4fca990d74758a5
Jason Canterbury <jason@flipsidecrypto.com>
2022-10-31 12:59:05 -0400

    Chainhead route fix

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,6 +1,6 @@
 {% macro create_udf_get_chainhead() %}
     CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_polygon_api AS {% if target.name == "prod" %}
-        'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod//get_chainhead'
+        'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
         'https://jml4wcap5f.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {%- endif %};
