e41e665d8503ec0d46c7b2db56dee7d4e1fe9137
drethereum <trevor.wenokur@gmail.com>
2022-11-11 11:08:20 -0700

    added project level variable and associated IF statements to on-run-start macros to default UDF/SP updates to False

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,22 +1,24 @@
 {% macro create_udfs() %}
-    {% set sql %}
-    CREATE schema if NOT EXISTS silver;
-{{ create_js_hex_to_int() }};
-{{ create_udf_hex_to_int(
-        schema = "public"
-    ) }}
-    {{ create_udtf_get_base_table(
-        schema = "streamline"
-    ) }}
-
-    {% endset %}
-    {% do run_query(sql) %}
-    {% if target.database != "POLYGON_COMMUNITY_DEV" %}
+    {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
-        {{ create_udf_get_chainhead() }}
-        {{ create_udf_get_blocks() }}
+        CREATE schema if NOT EXISTS silver;
+    {{ create_js_hex_to_int() }};
+    {{ create_udf_hex_to_int(
+            schema = "public"
+        ) }}
+        {{ create_udtf_get_base_table(
+            schema = "streamline"
+        ) }}
 
         {% endset %}
         {% do run_query(sql) %}
+        {% if target.database != "POLYGON_COMMUNITY_DEV" %}
+            {% set sql %}
+            {{ create_udf_get_chainhead() }}
+            {{ create_udf_get_blocks() }}
+
+            {% endset %}
+            {% do run_query(sql) %}
+        {% endif %}
     {% endif %}
 {% endmacro %}
