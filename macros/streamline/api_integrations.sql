2c78ef429f53c1361779c6648e442ba24b4bdfed
cantjaso <108745501+cantjaso@users.noreply.github.com>
2022-11-16 15:31:24 -0500

    Jason/new gateways (#47)
    * New api gateways
    
    * Chainhead fix

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -2,8 +2,8 @@
     {% if target.name == "prod" %}
         {% set sql %}
         CREATE api integration IF NOT EXISTS aws_polygon_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/snowflake-api-polygon' api_allowed_prefixes = (
-            'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod/',
-            'https://jml4wcap5f.execute-api.us-east-1.amazonaws.com/dev/'
+            'https://088pv40k78.execute-api.us-east-1.amazonaws.com/prod/',
+            'https://ug2z7nx4bi.execute-api.us-east-1.amazonaws.com/dev/'
         ) enabled = TRUE;
 {% endset %}
         {% do run_query(sql) %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,5 +1,5 @@
 {% macro create_udf_get_chainhead() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_polygon_api AS {% if target.name == "prod" %}
+    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_get_chainhead() returns variant api_integration = aws_polygon_api AS {% if target.name == "prod" %}
         'https://088pv40k78.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
         'https://ug2z7nx4bi.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
@@ -7,8 +7,7 @@
 {% endmacro %}
 
 {% macro create_udf_get_blocks() %}
-    CREATE
-    OR REPLACE EXTERNAL FUNCTION streamline.udf_get_blocks(
+    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_get_blocks(
         json variant
     ) returns text api_integration = aws_polygon_api AS {% if target.name == "prod" %}
         'https://088pv40k78.execute-api.us-east-1.amazonaws.com/prod/bulk_get_blocks'
