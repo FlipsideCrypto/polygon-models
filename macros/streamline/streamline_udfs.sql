d173f78b77ef967ef374b417c6c51edc0cc84627
cantjaso <108745501+cantjaso@users.noreply.github.com>
2022-11-16 11:34:43 -0500

    New api gateways (#46)

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,8 +1,8 @@
 {% macro create_udf_get_chainhead() %}
     CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_get_chainhead() returns variant api_integration = aws_polygon_api AS {% if target.name == "prod" %}
-        'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
+        'https://088pv40k78.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
-        'https://jml4wcap5f.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
+        'https://ug2z7nx4bi.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {%- endif %};
 {% endmacro %}
 
@@ -11,8 +11,8 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_get_blocks(
         json variant
     ) returns text api_integration = aws_polygon_api AS {% if target.name == "prod" %}
-        'https://avl1rax159.execute-api.us-east-1.amazonaws.com/prod/bulk_get_blocks'
+        'https://088pv40k78.execute-api.us-east-1.amazonaws.com/prod/bulk_get_blocks'
     {% else %}
-        'https://jml4wcap5f.execute-api.us-east-1.amazonaws.com/dev/bulk_get_blocks'
+        'https://ug2z7nx4bi.execute-api.us-east-1.amazonaws.com/dev/bulk_get_blocks'
     {%- endif %};
 {% endmacro %}
