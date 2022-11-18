73c4afad6e3c0d62adea1e00d4e2713e3bd200cc
Austin <93135983+austinFlipside@users.noreply.github.com>
2022-11-17 19:42:46 -0500

    fix task (#50)

--- macros/create_tasks.sql
+++ macros/create_tasks.sql
@@ -1,9 +1,7 @@
 {% macro create_tasks() %}
   {% if var("UPDATE_UDFS_AND_SPS") %}
     {% if target.database == 'POLYGON' %}
-        {{ task_get_abis('resume') }};
-    {% else %}
-        {{ task_get_abis('suspend') }};
+        {{ task_get_abis() }};
     {% endif %}
 {% endif %}
 {% endmacro %}
