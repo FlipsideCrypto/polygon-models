000bc87fe545d5323cd6d71f2e21fa2758fe0f8e
Austin <austin@flipsidecrypto.com>
2022-11-30 11:20:05 -0500

    60

--- macros/api_udf/task_get_abis.sql
+++ macros/api_udf/task_get_abis.sql
@@ -4,7 +4,7 @@
     'create or replace task bronze_api.get_block_explorer_abis
     warehouse = DBT_CLOUD_POLYGON
     allow_overlapping_execution = false 
-    schedule = \'120 minute\' 
+    schedule = \'60 minute\' 
     as 
     BEGIN 
 INSERT INTO
