f69036acb08d2747f144547b444ef9e7844f67d1
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-01-04 13:53:07 -0500

    An 2604/polygon optimize (#71)
    * optimize polygon
    
    * tests + docs
    
    * change SO

--- models/silver/silver__traces.sql
+++ models/silver/silver__traces.sql
@@ -2,7 +2,8 @@
     materialized = 'incremental',
     unique_key = 'tx_hash',
     cluster_by = ['block_timestamp::DATE'],
-    incremental_strategy = 'delete+insert'
+    incremental_strategy = 'delete+insert',
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
 WITH new_blocks AS (
--- models/silver/silver__transactions.sql
+++ models/silver/silver__transactions.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized = 'incremental',
     unique_key = "tx_hash",
-    cluster_by = ['block_timestamp::DATE']
+    cluster_by = ['block_timestamp::DATE'],
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION"
 ) }}
 
 WITH base_table AS (
