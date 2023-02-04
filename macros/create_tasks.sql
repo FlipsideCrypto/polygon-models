3dec0d52e1d43f7f99015e92d29f0764a7e3db05
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-03 20:37:04 -0500

    Actions issues

--- macros/create_tasks.sql
+++ macros/create_tasks.sql
@@ -1,7 +1,7 @@
 {% macro create_tasks() %}
   {% if var("UPDATE_UDFS_AND_SPS") %}
     {% if target.database == 'POLYGON' %}
-        {{ task_get_abis() }};
+        {# {{ task_get_abis() }}; #}
     {% endif %}
 {% endif %}
 {% endmacro %}
