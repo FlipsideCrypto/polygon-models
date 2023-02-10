a1eb72ce9ab5c2136432c68826668adaedac6e6a
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-10 13:28:14 -0500

    Turn on job, batch sizes

--- models/silver/streamline/streamline__trace_blocks_realtime.sql
+++ models/silver/streamline/streamline__trace_blocks_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'trace_blocks', 'sql_limit', {{var('sql_limit','30000')}}, 'producer_batch_size', {{var('producer_batch_size','30000')}}, 'worker_batch_size', {{var('worker_batch_size','30000')}}, 'batch_call_limit', {{var('batch_call_limit','99')}}))",
+        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'trace_blocks', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','10000')}}, 'worker_batch_size', {{var('worker_batch_size','10000')}}, 'batch_call_limit', {{var('batch_call_limit','99')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
