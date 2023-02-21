0b668961f332e78f189f103618e394e60012f630
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-21 13:39:16 -0500

    Add separate workflow files and update limit sizing (#105)
    * Add separate workflow files and update limit sizing
    
    * Remove copy in name

--- models/silver/streamline/streamline__trace_blocks_history.sql
+++ models/silver/streamline/streamline__trace_blocks_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'trace_blocks', 'sql_limit', {{var('sql_limit','1000000')}}, 'producer_batch_size', {{var('producer_batch_size','40000')}}, 'worker_batch_size', {{var('worker_batch_size','10000')}}, 'batch_call_limit', {{var('batch_call_limit','99')}}))",
+        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'trace_blocks', 'sql_limit', {{var('sql_limit','10000000')}}, 'producer_batch_size', {{var('producer_batch_size','20000')}}, 'worker_batch_size', {{var('worker_batch_size','10000')}}, 'batch_call_limit', {{var('batch_call_limit','99')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
