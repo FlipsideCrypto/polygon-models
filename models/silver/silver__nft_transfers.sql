7afe6decf244d66c7cf8eea939679cdda93e3e49
austin <austin@flipsidecrypto.com>
2023-02-28 17:23:18 -0500

    remove punk nonsense

--- models/silver/silver__nft_transfers.sql
+++ models/silver/silver__nft_transfers.sql
@@ -96,87 +96,6 @@ transfer_singles AS (
     WHERE
         topics [0] :: STRING = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62'
 ),
-punks_bought AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        PUBLIC.udf_hex_to_int(
-            topics [1] :: STRING
-        ) AS token_id,
-        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS from_address,
-        CONCAT('0x', SUBSTR(topics [3] :: STRING, 27, 40)) AS to_address,
-        NULL AS erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        base
-    WHERE
-        topics [0] :: STRING = '0x58e5d5a525e3b40bc15abaa38b5882678db1ee68befd2f60bafe3a7fd06db9e3'
-),
-punks_transfer AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        PUBLIC.udf_hex_to_int(
-            DATA :: STRING
-        ) AS token_id,
-        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
-        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address,
-        NULL AS erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        base
-    WHERE
-        topics [0] :: STRING = '0x05af636b70da6819000c49f85b21fa82081c632069bb626f30932034099107d8'
-),
-regular_punk_transfers AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS from_address,
-        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS to_address,
-        _inserted_timestamp,
-        event_index
-    FROM
-        base
-    WHERE
-        topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-        AND contract_address IN (
-            '0x6ba6f2207e343923ba692e5cae646fb0f566db8d',
-            '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
-        )
-),
-legacy_tokens AS (
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        CONCAT('0x', SUBSTR(segmented_data [0], 25, 40)) AS from_address,
-        CONCAT('0x', SUBSTR(segmented_data [1], 25, 40)) AS to_address,
-        PUBLIC.udf_hex_to_int(
-            segmented_data [2] :: STRING
-        ) AS token_id,
-        NULL AS erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        base
-    WHERE
-        topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
-        AND topics [1] IS NULL
-),
 all_transfers AS (
     SELECT
         _log_id,
@@ -207,51 +126,6 @@ all_transfers AS (
         event_index
     FROM
         transfer_singles
-    UNION ALL
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        from_address,
-        to_address,
-        token_id,
-        erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        punks_bought
-    UNION ALL
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        from_address,
-        to_address,
-        token_id,
-        erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        punks_transfer
-    UNION ALL
-    SELECT
-        _log_id,
-        block_number,
-        tx_hash,
-        block_timestamp,
-        contract_address,
-        from_address,
-        to_address,
-        token_id,
-        erc1155_value,
-        _inserted_timestamp,
-        event_index
-    FROM
-        legacy_tokens
 )
 SELECT
     block_number,
