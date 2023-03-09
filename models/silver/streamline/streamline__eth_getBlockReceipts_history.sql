8eb6a00d551276da5b0d35cc302c7be19b443523
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-03-09 13:41:52 -0500

    Increase run frequency and decrease load (#127)

--- models/silver/streamline/streamline__eth_getBlockReceipts_history.sql
+++ models/silver/streamline/streamline__eth_getBlockReceipts_history.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'eth_getBlockReceipts', 'sql_limit', {{var('sql_limit','10000000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}, 'batch_call_limit', {{var('batch_call_limit','1000')}}))",
+        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'eth_getBlockReceipts', 'sql_limit', {{var('sql_limit','5000000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}, 'batch_call_limit', {{var('batch_call_limit','1000')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
