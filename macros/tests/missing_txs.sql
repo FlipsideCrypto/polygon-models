9a1b6a29895cd3969e2ca23d61e8013e504a213a
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-04-23 17:16:58 -0400

    move qualify (#147)
    * move qualify
    
    * move qualify
    
    * dupes
    
    * sort

--- macros/tests/missing_txs.sql
+++ macros/tests/missing_txs.sql
@@ -1,4 +1,4 @@
-{% macro missing_txs(
+{% macro missing_receipts(
         model
     ) %}
     WITH txs_base AS (
@@ -30,7 +30,7 @@ WHERE
     OR model_block_number IS NULL
 {% endmacro %}
 
-{% macro recent_missing_txs(
+{% macro recent_missing_receipts(
         model
     ) %}
     WITH txs_base AS (
@@ -39,6 +39,76 @@ WHERE
             tx_hash AS base_tx_hash
         FROM
             {{ ref('test_silver__transactions_recent') }}
+    ),
+    model_name AS (
+        SELECT
+            block_number AS model_block_number,
+            tx_hash AS model_tx_hash
+        FROM
+            {{ model }}
+    )
+SELECT
+    base_block_number,
+    base_tx_hash,
+    model_block_number,
+    model_tx_hash
+FROM
+    txs_base
+    LEFT JOIN model_name
+    ON base_block_number = model_block_number
+    AND base_tx_hash = model_tx_hash
+WHERE
+    model_tx_hash IS NULL
+    OR model_block_number IS NULL
+{% endmacro %}
+
+{% macro missing_traces(
+        model
+    ) %}
+    WITH txs_base AS (
+        SELECT
+            block_number AS base_block_number,
+            tx_hash AS base_tx_hash
+        FROM
+            {{ ref('test_silver__transactions_full') }}
+        WHERE
+            from_address <> '0x0000000000000000000000000000000000000000'
+            AND to_address <> '0x0000000000000000000000000000000000000000'
+    ),
+    model_name AS (
+        SELECT
+            block_number AS model_block_number,
+            tx_hash AS model_tx_hash
+        FROM
+            {{ model }}
+    )
+SELECT
+    base_block_number,
+    base_tx_hash,
+    model_block_number,
+    model_tx_hash
+FROM
+    txs_base
+    LEFT JOIN model_name
+    ON base_block_number = model_block_number
+    AND base_tx_hash = model_tx_hash
+WHERE
+    model_tx_hash IS NULL
+    OR model_block_number IS NULL
+{% endmacro %}
+
+{% macro recent_missing_traces(
+        model
+    ) %}
+    WITH txs_base AS (
+        SELECT
+            block_number AS base_block_number,
+            tx_hash AS base_tx_hash
+        FROM
+            {{ ref('test_silver__transactions_recent') }}
+        WHERE
+            from_address <> '0x0000000000000000000000000000000000000000'
+            AND to_address <> '0x0000000000000000000000000000000000000000'
     ),
     model_name AS (
         SELECT
--- tests/polygon/test_silver__missing_traces.sql
+++ tests/polygon/test_silver__missing_traces.sql
@@ -1,2 +1,2 @@
 -- depends_on: {{ ref('test_silver__transactions_full') }}
-{{ missing_txs(ref("test_silver__traces_full")) }}
+{{ missing_traces(ref("test_silver__traces_full")) }}
