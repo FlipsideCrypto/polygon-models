9cc46feb8f323769703d1e3c81fc1869a64b6d9a
drethereum <trevor.wenokur@gmail.com>
2023-04-24 12:02:52 -0600

    uniswap v3

--- models/silver/dex/silver_dex__complete_dex_swaps.sql
+++ models/silver/dex/silver_dex__complete_dex_swaps.sql
@@ -40,6 +40,116 @@ AND hour >= (
 {% endif %}
 ), 
 
+univ3_swaps AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    pool_address AS contract_address,
+    CONCAT(
+      c1.symbol,
+      '-',
+      c2.symbol,
+      ' ',
+      fee,
+      ' ',
+      tick_spacing
+    ) AS pool_name,
+    'Swap' AS event_name,
+    amount0_unadj / pow(10, COALESCE(c1.decimals, 18)) AS amount0_adjusted,
+    amount1_unadj / pow(10, COALESCE(c2.decimals, 18)) AS amount1_adjusted,
+    CASE
+      WHEN c1.decimals IS NOT NULL THEN ROUND(
+        p1.price * amount0_adjusted,
+        2
+      )
+    END AS amount0_usd,
+    CASE
+      WHEN c2.decimals IS NOT NULL THEN ROUND(
+        p2.price * amount1_adjusted,
+        2
+      )
+    END AS amount1_usd,
+    CASE
+      WHEN amount0_unadj > 0 THEN ABS(amount0_unadj)
+      ELSE ABS(amount1_unadj)
+    END AS amount_in_unadj,
+    CASE
+      WHEN amount0_unadj > 0 THEN ABS(amount0_adjusted)
+      ELSE ABS(amount1_adjusted)
+    END AS amount_in,
+    CASE
+      WHEN amount0_unadj > 0 THEN ABS(amount0_usd)
+      ELSE ABS(amount1_usd)
+    END AS amount_in_usd,
+    CASE
+      WHEN amount0_unadj < 0 THEN ABS(amount0_unadj)
+      ELSE ABS(amount1_unadj)
+    END AS amount_out_unadj,
+    CASE
+      WHEN amount0_unadj < 0 THEN ABS(amount0_adjusted)
+      ELSE ABS(amount1_adjusted)
+    END AS amount_out,
+    CASE
+      WHEN amount0_unadj < 0 THEN ABS(amount0_usd)
+      ELSE ABS(amount1_usd)
+    END AS amount_out_usd,
+    sender,
+    recipient AS tx_to,
+    event_index,
+    'uniswap-v3' AS platform,
+    CASE
+      WHEN amount0_unadj > 0 THEN token0_address
+      ELSE token1_address
+    END AS token_in,
+    CASE
+      WHEN amount0_unadj < 0 THEN token0_address
+      ELSE token1_address
+    END AS token_out,
+    CASE
+      WHEN amount0_unadj > 0 THEN c1.symbol
+      ELSE c2.symbol
+    END AS symbol_in,
+    CASE
+      WHEN amount0_unadj < 0 THEN c1.symbol
+      ELSE c2.symbol
+    END AS symbol_out,
+    _log_id,
+    _inserted_timestamp
+  FROM
+    {{ ref('silver_dex__univ3_swaps') }}
+    s
+    LEFT JOIN contracts c1
+    ON c1.address = s.token0_address
+    LEFT JOIN contracts c2
+    ON c2.address = s.token1_address
+    LEFT JOIN prices p1
+    ON s.token0_address = p1.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p1.hour
+    LEFT JOIN prices p2
+    ON s.token1_address = p2.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p2.hour
+
+{% if is_incremental() %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) :: DATE
+    FROM
+      {{ this }}
+  )
+{% endif %}
+),
+
 {# woofi_swaps AS (
   SELECT
     block_number,
@@ -340,7 +450,6 @@ WHERE
 {% endif %}
 ),
 
-{#
 curve_swaps AS (
   SELECT
     block_number,
@@ -400,7 +509,7 @@ AND
       {{ this }}
   )
 {% endif %}
-), #}
+),
 
 balancer_swaps AS (
   SELECT
@@ -512,6 +621,67 @@ SELECT
   _inserted_timestamp
 FROM
   sushi_swaps
+UNION ALL 
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  event_name,
+  amount_in_unadj,
+  amount_out_unadj,
+  amount_in,
+  amount_out,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  token_in,
+  token_out,
+  symbol_in,
+  symbol_out,
+  decimals_in,
+  decimals_out,
+  _log_id,
+  _inserted_timestamp
+FROM
+  curve_swaps
+),
+
+--union all non-standard dex CTEs here (excludes amount_usd)
+all_dex_custom AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    pool_name,
+    event_name,
+    amount_in_unadj,
+    amount_in,
+    amount_in_usd,
+    amount_out_unadj,
+    amount_out,
+    amount_out_usd,
+    sender,
+    tx_to,
+    event_index,
+    platform,
+    token_in,
+    token_out,
+    symbol_in,
+    symbol_out,
+    _log_id,
+    _inserted_timestamp
+  FROM
+    univ3_swaps
 ),
 
 FINAL AS (
@@ -556,6 +726,41 @@ FINAL AS (
   LEFT JOIN prices p2
     ON s.token_out = p2.token_address
       AND DATE_TRUNC('hour', block_timestamp) = p2.hour
+UNION ALL
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    pool_name,
+    event_name,
+    amount_in_unadj,
+    amount_in,
+    ROUND(
+      amount_in_usd,
+      2
+    ) AS amount_in_usd,
+    amount_out_unadj,
+    amount_out,
+    ROUND(
+      amount_out_usd,
+      2
+    ) AS amount_out_usd,
+    sender,
+    tx_to,
+    event_index,
+    platform,
+    token_in,
+    token_out,
+    symbol_in,
+    symbol_out,
+    _log_id,
+    _inserted_timestamp
+  FROM
+    all_dex_custom C
 )
 
 SELECT
