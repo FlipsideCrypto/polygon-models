f26899e280e9de47e03a5a1455e49c97d7b78b89
drethereum <71602799+drethereum@users.noreply.github.com>
2023-05-04 12:36:34 -0600

    add/shares-ref-usage (#162)
    * added models for shares ref
    
    * Removed legacy prices
    
    * nft change
    
    * adds lookback
    
    ---------
    
    Co-authored-by: sam <sam@flipsidecrypto.com>

--- models/gold/NFT/core__ez_nft_sales.sql
+++ models/gold/NFT/core__ez_nft_sales.sql
@@ -5,6 +5,107 @@
     meta ={ 'database_tags':{ 'table':{ 'PURPOSE': 'NFT' }} }
 ) }}
 
+WITH base AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_type,
+        platform_address,
+        platform_name,
+        platform_exchange_version,
+        seller_address,
+        buyer_address,
+        nft_address,
+        erc1155_value,
+        tokenId,
+        currency_address,
+        price AS price_raw,
+        total_fees AS total_fees_raw,
+        platform_fee AS platform_fee_raw,
+        creator_fee AS creator_fee_raw,
+        tx_fee,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature
+    FROM
+        {{ ref('silver__seaport_1_1') }}
+    UNION ALL
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_type,
+        platform_address,
+        platform_name,
+        platform_exchange_version,
+        seller_address,
+        buyer_address,
+        nft_address,
+        erc1155_value,
+        tokenId,
+        currency_address,
+        price AS price_raw,
+        total_fees AS total_fees_raw,
+        platform_fee AS platform_fee_raw,
+        creator_fee AS creator_fee_raw,
+        tx_fee,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature
+    FROM
+        {{ ref('silver__seaport_1_4') }}
+),
+all_prices AS (
+    SELECT
+        HOUR,
+        token_address,
+        symbol,
+        price AS hourly_price,
+        decimals
+    FROM
+        {{ ref('silver__prices') }}
+    WHERE
+        HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                base
+        )
+    UNION ALL
+    SELECT
+        HOUR,
+        'MATIC' AS token_address,
+        'MATIC' AS symbol,
+        price AS hourly_price,
+        decimals
+    FROM
+        {{ ref('silver__prices') }}
+    WHERE
+        HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                base
+        )
+        AND token_address = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+),
+matic_price AS (
+    SELECT
+        HOUR,
+        price AS matic_hourly_price
+    FROM
+        {{ ref('silver__prices') }}
+    WHERE
+        HOUR :: DATE IN (
+            SELECT
+                DISTINCT block_timestamp :: DATE
+            FROM
+                base
+        )
+        AND token_address = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+)
 SELECT
     block_number,
     block_timestamp,
@@ -21,282 +122,87 @@ SELECT
     tokenId,
     CASE
         WHEN currency_address = 'MATIC' THEN 'MATIC'
-        ELSE cc2.token_symbol
+        ELSE p.symbol
     END AS currency_symbol,
     currency_address,
-    s1.price / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
-        )
-    ) AS price,
     CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s1.price / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS price_usd,
-    total_fees / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
+        WHEN currency_address IN (
+            'MATIC',
+            '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ) THEN price_raw / pow(
+            10,
+            18
         )
-    ) AS total_fees,
-    platform_fee / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
+        ELSE COALESCE (price_raw / pow(10, decimals), price_raw)
+    END AS price,
+    IFF(
+        decimals IS NULL,
+        0,
+        price * hourly_price
+    ) AS price_usd,
+    CASE
+        WHEN currency_address IN (
+            'MATIC',
+            '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ) THEN total_fees_raw / pow(
+            10,
+            18
         )
-    ) AS platform_fee,
-    creator_fee / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
+        ELSE COALESCE (total_fees_raw / pow(10, decimals), total_fees_raw)
+    END AS total_fees,
+    CASE
+        WHEN currency_address IN (
+            'MATIC',
+            '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ) THEN platform_fee_raw / pow(
+            10,
+            18
         )
-    ) AS creator_fee,
+        ELSE COALESCE (platform_fee_raw / pow(10, decimals), platform_fee_raw)
+    END AS platform_fee,
     CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s1.total_fees / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS total_fees_usd,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s1.platform_fee / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS platform_fee_usd,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s1.creator_fee / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS creator_fee_usd,
+        WHEN currency_address IN (
+            'MATIC',
+            '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ) THEN creator_fee_raw / pow(
+            10,
+            18
+        )
+        ELSE COALESCE (creator_fee_raw / pow(10, decimals), creator_fee_raw)
+    END AS creator_fee,
+    IFF(
+        decimals IS NULL,
+        0,
+        total_fees * hourly_price
+    ) AS total_fees_usd,
+    IFF(
+        decimals IS NULL,
+        0,
+        platform_fee * hourly_price
+    ) AS platform_fee_usd,
+    IFF(
+        decimals IS NULL,
+        0,
+        creator_fee * hourly_price
+    ) AS creator_fee_usd,
     tx_fee,
-    tx_fee * p2.price AS tx_fee_usd,
+    tx_fee * matic_hourly_price AS tx_fee_usd,
     origin_from_address,
     origin_to_address,
     origin_function_signature
 FROM
-    {{ ref('silver__seaport_1_1') }}
-    s1
+    base b
+    LEFT JOIN all_prices p
+    ON DATE_TRUNC(
+        'hour',
+        b.block_timestamp
+    ) = p.hour
+    AND b.currency_address = p.token_address
+    LEFT JOIN matic_price m
+    ON DATE_TRUNC(
+        'hour',
+        b.block_timestamp
+    ) = m.hour
     LEFT JOIN {{ ref('silver__contracts') }}
     cc1
-    ON s1.nft_address = cc1.contract_address
-    LEFT JOIN {{ ref('silver__contracts') }}
-    cc2
-    ON s1.currency_address = cc2.contract_address
-    LEFT JOIN {{ ref('silver__prices') }}
-    p1
-    ON s1.currency_address = p1.matic_address
-    AND DATE_TRUNC(
-        'hour',
-        s1.block_timestamp
-    ) = p1.hour
-    LEFT JOIN {{ ref('silver__prices') }}
-    p2
-    ON p2.matic_address = 'MATIC'
-    AND DATE_TRUNC(
-        'hour',
-        s1.block_timestamp
-    ) = p2.hour
-UNION ALL
-SELECT
-    block_number,
-    block_timestamp,
-    tx_hash,
-    event_type,
-    platform_address,
-    platform_name,
-    platform_exchange_version,
-    seller_address,
-    buyer_address,
-    nft_address,
-    cc1.token_name AS project_name,
-    erc1155_value,
-    tokenId,
-    CASE
-        WHEN currency_address = 'MATIC' THEN 'MATIC'
-        ELSE cc2.token_symbol
-    END AS currency_symbol,
-    currency_address,
-    s4.price / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
-        )
-    ) AS price,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s4.price / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS price_usd,
-    total_fees / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
-        )
-    ) AS total_fees,
-    platform_fee / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
-        )
-    ) AS platform_fee,
-    creator_fee / pow(
-        10,
-        COALESCE(
-            CASE
-                WHEN currency_address = 'MATIC' THEN 18
-                ELSE cc2.token_decimals
-            END,
-            0
-        )
-    ) AS creator_fee,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s4.total_fees / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS total_fees_usd,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s4.platform_fee / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS platform_fee_usd,
-    CASE
-        WHEN currency_address = 'MATIC'
-        OR cc2.token_decimals IS NOT NULL THEN (
-            s4.creator_fee / pow(
-                10,
-                COALESCE(
-                    CASE
-                        WHEN currency_address = 'MATIC' THEN 18
-                        ELSE cc2.token_decimals
-                    END,
-                    0
-                )
-            )
-        ) * p1.price
-    END AS creator_fee_usd,
-    tx_fee,
-    tx_fee * p2.price AS tx_fee_usd,
-    origin_from_address,
-    origin_to_address,
-    origin_function_signature
-FROM
-    {{ ref('silver__seaport_1_4') }}
-    s4
-    LEFT JOIN {{ ref('silver__contracts') }}
-    cc1
-    ON s4.nft_address = cc1.contract_address
-    LEFT JOIN {{ ref('silver__contracts') }}
-    cc2
-    ON s4.currency_address = cc2.contract_address
-    LEFT JOIN {{ ref('silver__prices') }}
-    p1
-    ON s4.currency_address = p1.matic_address
-    AND DATE_TRUNC(
-        'hour',
-        s4.block_timestamp
-    ) = p1.hour
-    LEFT JOIN {{ ref('silver__prices') }}
-    p2
-    ON p2.matic_address = 'MATIC'
-    AND DATE_TRUNC(
-        'hour',
-        s4.block_timestamp
-    ) = p2.hour
+    ON b.nft_address = cc1.contract_address
