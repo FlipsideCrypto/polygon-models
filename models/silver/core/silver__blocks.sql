ff3df79dc5e80da13ccdfd09ef76ace198a951ef
mattromano <mattromano@flipsidecrypto.com>
2023-06-26 21:17:57 -0700

    missed public. schemas

--- models/silver/core/silver__blocks.sql
+++ models/silver/core/silver__blocks.sql
@@ -7,39 +7,39 @@
 
 SELECT
     block_number,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :baseFeePerGas :: STRING
     ) :: INT AS base_fee_per_gas,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :difficulty :: STRING
     ) :: INT AS difficulty,
     DATA :extraData :: STRING AS extra_data,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :gasLimit :: STRING
     ) :: INT AS gas_limit,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :gasUsed :: STRING
     ) :: INT AS gas_used,
     DATA :hash :: STRING AS HASH,
     DATA :logsBloom :: STRING AS logs_bloom,
     DATA :miner :: STRING AS miner,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :nonce :: STRING
     ) :: INT AS nonce,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :number :: STRING
     ) :: INT AS NUMBER,
     DATA :parentHash :: STRING AS parent_hash,
     DATA :receiptsRoot :: STRING AS receipts_root,
     DATA :sha3Uncles :: STRING AS sha3_uncles,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :size :: STRING
     ) :: INT AS SIZE,
     DATA :stateRoot :: STRING AS state_root,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :timestamp :: STRING
     ) :: TIMESTAMP AS block_timestamp,
-    PUBLIC.udf_hex_to_int(
+    utils.udf_hex_to_int(
         DATA :totalDifficulty :: STRING
     ) :: INT AS total_difficulty,
     DATA :transactionsRoot :: STRING AS transactions_root,
