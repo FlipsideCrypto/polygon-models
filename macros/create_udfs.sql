25f164ea127221ed55257f9bb913172d668e0463
mattromano <mattromano88@gmail.com>
2023-07-13 10:53:53 -0700

    update package to 1.4, re-point macros and tests

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,10 +2,6 @@
     {% if var("UPDATE_UDFS_AND_SPS") %}
         {% set sql %}
         CREATE schema if NOT EXISTS silver;
-    {{ create_js_hex_to_int() }};
-    {{ create_udf_hex_to_int(
-            schema = "public"
-        ) }}
         {{ create_udtf_get_base_table(
             schema = "streamline"
         ) }}
