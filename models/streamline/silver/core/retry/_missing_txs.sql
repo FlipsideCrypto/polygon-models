960c1208656fbf2ff0955af7ff58fe7d037fb5e4
drethereum <trevor.wenokur@gmail.com>
2023-09-28 12:21:06 -0600

    revert missing tx logic and add null filter to missing receipts

--- models/streamline/silver/core/retry/_missing_txs.sql
+++ models/streamline/silver/core/retry/_missing_txs.sql
@@ -9,7 +9,7 @@ WITH lookback AS (
     FROM
         {{ ref("_block_lookback") }}
 ),
-tx_position AS (
+transactions AS (
     SELECT
         block_number,
         POSITION,
@@ -31,58 +31,10 @@ tx_position AS (
             FROM
                 lookback
         )
-),
-tx_count AS (
-    SELECT
-        block_number,
-        MAX(POSITION) AS num_tx
-    FROM
-        {{ ref("silver__transactions") }}
-    WHERE
-        block_timestamp >= DATEADD('hour', -84, SYSDATE())
-        AND block_number >= (
-            SELECT
-                block_number
-            FROM
-                lookback
-        )
-    GROUP BY
-        1
-),
-receipt_count AS (
-    SELECT
-        block_number,
-        MAX(POSITION) AS num_r
-    FROM
-        {{ ref("silver__receipts") }}
-    WHERE
-        _inserted_timestamp >= DATEADD('hour', -84, SYSDATE())
-        AND block_number >= (
-            SELECT
-                block_number
-            FROM
-                lookback
-        )
-    GROUP BY
-        1
-),
-all_blocks AS (
-    SELECT
-        DISTINCT block_number AS block_number
-    FROM
-        tx_position
-    WHERE
-        POSITION - prev_POSITION <> 1
-    UNION
-    SELECT
-        DISTINCT block_number
-    FROM
-        tx_count t
-        JOIN receipt_count r USING(block_number)
-    WHERE
-        num_tx <> num_r
 )
 SELECT
-    block_number
+    DISTINCT block_number AS block_number
 FROM
-    all_blocks
+    transactions
+WHERE
+    POSITION - prev_POSITION <> 1
