07506a7ce6c4758d71eba7ef6d919f0c4e27b63f
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-10-18 19:01:22 -0400

    revert (#262)

--- models/gold/core/core__ez_token_transfers.sql
+++ models/gold/core/core__ez_token_transfers.sql
@@ -8,24 +8,46 @@ SELECT
     block_number,
     block_timestamp,
     tx_hash,
-    event_index,
     origin_function_signature,
     origin_from_address,
     origin_to_address,
-    contract_address,
+    t.contract_address,
     from_address,
     to_address,
-    raw_amount_precise,
     raw_amount,
-    amount_precise,
-    amount,
-    amount_usd,
-    decimals,
-    symbol,
-    token_price,
-    has_decimal,
-    has_price,
-    _log_id,
-    _inserted_timestamp
+    token_decimals AS decimals,
+    token_symbol AS symbol,
+    price AS token_price,
+    CASE
+        WHEN C.token_decimals IS NOT NULL THEN raw_amount / pow(
+            10,
+            C.token_decimals
+        )
+        ELSE NULL
+    END AS amount,
+    CASE
+        WHEN C.token_decimals IS NOT NULL
+        AND price IS NOT NULL THEN amount * price
+        ELSE NULL
+    END AS amount_usd,
+    CASE
+        WHEN C.token_decimals IS NULL THEN 'false'
+        ELSE 'true'
+    END AS has_decimal,
+    CASE
+        WHEN price IS NULL THEN 'false'
+        ELSE 'true'
+    END AS has_price,
+    _log_id
 FROM
-    {{ ref('silver__transfers') }}
+    {{ ref('core__fact_token_transfers') }}
+    t
+    LEFT JOIN {{ ref('price__ez_hourly_token_prices') }}
+    p
+    ON t.contract_address = p.token_address
+    AND DATE_TRUNC(
+        'hour',
+        t.block_timestamp
+    ) = HOUR
+    LEFT JOIN {{ ref('silver__contracts') }} C
+    ON t.contract_address = C.contract_address
