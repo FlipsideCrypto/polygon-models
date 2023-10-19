62c3a928dc851304bd522a7f935c1e6abfe31307
Austin <austin@flipsidecrypto.com>
2023-10-19 14:48:13 -0400

    update

--- macros/create_gha_tasks.sql
+++ macros/create_gha_tasks.sql
@@ -1,5 +1,5 @@
 {% macro create_gha_tasks() %}
-  {% if var("CREATE_GHA_TASKS") %}
+  {% if var("UPDATE_GHA_TASKS") %}
     {% if target.database == 'POLYGON' %}
          {{ trigger_dummy_gha() }}; 
     {% endif %}
