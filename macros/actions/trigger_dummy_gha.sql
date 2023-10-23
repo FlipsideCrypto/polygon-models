f67ed8897ddf2a70e2403594c164f96f0c4df13c
Austin <austin@flipsidecrypto.com>
2023-10-23 15:53:52 -0400

    set result

--- macros/actions/trigger_dummy_gha.sql
+++ macros/actions/trigger_dummy_gha.sql
@@ -1,25 +1,26 @@
 {% macro trigger_dummy_gha() %}
   {% set sql %}
     EXECUTE IMMEDIATE 
-    'CREATE OR REPLACE task silver.trigger_dummy_workflow 
-    warehouse = DBT_CLOUD
-    schedule = \'USING CRON 0,20,40 * * * * UTC\' 
-    COMMENT = \'trigger dummy workflow\' AS
-select
-    github_actions.workflow_dispatches(
-        \'FlipsideCrypto\',
-        \'polygon-models\',
-        \'dbt_run_dummy.yml\',
-        NULL
-    );'
+    'CREATE OR REPLACE TASK silver.trigger_dummy_workflow 
+    WAREHOUSE = DBT_CLOUD
+    SCHEDULE = \'USING CRON 0,20,40 * * * * UTC\' 
+    COMMENT = \'Trigger Dummy Workflow\' AS
+DECLARE
+  rs resultset;
+  output string;
+BEGIN
+  rs := (SELECT github_actions.workflow_dispatches(\'FlipsideCrypto\', \'polygon-models\', \'dbt_run_dummy.yml\', NULL)::status_code::int AS status_code);
+  SELECT LISTAGG($1, ';') INTO :output FROM TABLE(result_scan(LAST_QUERY_ID())) LIMIT 1;
+  CALL SYSTEM$SET_RETURN_VALUE(:output);
+END;'
 {% endset %}
-    {% do run_query(sql) %}
+  {% do run_query(sql) %}
 
-{% if target.database.upper() == 'POLYGON' %}
+  {% if target.database.upper() == 'POLYGON' %}
     {% set sql %}
-        alter task silver.trigger_dummy_workflow resume;
+      ALTER TASK silver.trigger_dummy_workflow RESUME;
     {% endset %}
     {% do run_query(sql) %}
-{% endif %}
+  {% endif %}
 
 {% endmacro %}
