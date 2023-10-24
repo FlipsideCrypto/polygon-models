2d0a581dff00ad02c4f45f5270ad2f38f7647bb8
Austin <austin@flipsidecrypto.com>
2023-10-24 17:58:12 -0400

    macro

--- macros/tests/workflows.sql
+++ macros/tests/workflows.sql
@@ -58,3 +58,23 @@ SELECT
 FROM
     base
 {% endmacro %}
+
+{% macro gha_task_results() %}
+SELECT
+    s.task_name,
+    s.workflow_name,
+    s.scheduled_time,
+    h.return_value
+FROM
+    {{ ref('silver__gha_task_schedule') }}
+    s
+    LEFT JOIN {{ ref('silver__gha_task_history') }}
+    h
+    ON s.task_name = h.task_name
+    AND TO_TIMESTAMP_NTZ(DATE_TRUNC('second', s.scheduled_time)) = TO_TIMESTAMP_NTZ(DATE_TRUNC('second', h.scheduled_time))
+    AND h.return_value = 204
+    AND h.state = 'SUCCEEDED'
+ORDER BY
+    task_name,
+    scheduled_time
+{% endmacro %}
--- models/silver/utilities/workflows/silver__gha_task_results.sql
+++ models/silver/utilities/workflows/silver__gha_task_results.sql
@@ -1,21 +1,4 @@
 {{ config(
     materialized = 'view'
 ) }}
-
-SELECT
-    s.task_name,
-    s.workflow_name,
-    s.scheduled_time,
-    h.return_value
-FROM
-    {{ ref('silver__gha_task_schedule') }}
-    s
-    LEFT JOIN {{ ref('silver__gha_task_history') }}
-    h
-    ON s.task_name = h.task_name
-    AND TO_TIMESTAMP_NTZ(DATE_TRUNC('second', s.scheduled_time)) = TO_TIMESTAMP_NTZ(DATE_TRUNC('second', h.scheduled_time))
-    AND h.return_value = 204
-    AND h.state = 'SUCCEEDED'
-ORDER BY
-    task_name,
-    scheduled_time
+{{ gha_task_results() }}
