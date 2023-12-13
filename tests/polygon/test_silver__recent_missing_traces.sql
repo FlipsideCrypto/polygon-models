19874e47718091f6acd19e08322ed10195644c55
Austin <austin@flipsidecrypto.com>
2023-12-13 09:01:40 -0500

    temp test

--- tests/polygon/test_silver__recent_missing_traces.sql
+++ tests/polygon/test_silver__recent_missing_traces.sql
@@ -1,2 +1,2 @@
 -- depends_on: {{ ref('test_silver__transactions_recent') }}
-{{ recent_missing_traces(ref("test_silver__traces_recent")) }}
+{{ recent_missing_traces2(ref("test_silver__traces_recent")) }}
