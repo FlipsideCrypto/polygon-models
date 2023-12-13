696d58c768694edeb3f10065b03e3f0fc15a765c
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-12-13 12:35:48 -0500

    temp test (#323)
    * temp test
    
    * missing traces threshold

--- macros/tests/missing_txs.sql
+++ macros/tests/missing_txs.sql
@@ -105,41 +105,6 @@ WHERE
     )
 {% endmacro %}
 
-{% macro recent_missing_traces(
-        model
-    ) %}
-    WITH txs_base AS (
-        SELECT
-            block_number AS base_block_number,
-            tx_hash AS base_tx_hash
-        FROM
-            {{ ref('test_silver__transactions_recent') }}
-        WHERE
-            from_address <> '0x0000000000000000000000000000000000000000'
-            AND to_address <> '0x0000000000000000000000000000000000000000'
-    ),
-    model_name AS (
-        SELECT
-            block_number AS model_block_number,
-            tx_hash AS model_tx_hash
-        FROM
-            {{ model }}
-    )
-SELECT
-    base_block_number,
-    base_tx_hash,
-    model_block_number,
-    model_tx_hash
-FROM
-    txs_base
-    LEFT JOIN model_name
-    ON base_block_number = model_block_number
-    AND base_tx_hash = model_tx_hash
-WHERE
-    model_tx_hash IS NULL
-    OR model_block_number IS NULL
-{% endmacro %}
-
 {% macro missing_confirmed_txs(
         model1,
         model2
@@ -175,5 +140,54 @@ WHERE
             MAX(base_block_number)
         FROM
             txs_base
+    )
+{% endmacro %}
+
+{% macro recent_missing_traces(
+        model,
+        threshold
+    ) %}
+    WITH txs_base AS (
+        SELECT
+            block_number AS base_block_number,
+            tx_hash AS base_tx_hash
+        FROM
+            {{ ref('test_silver__transactions_recent') }}
+        WHERE
+            from_address <> '0x0000000000000000000000000000000000000000'
+            AND to_address <> '0x0000000000000000000000000000000000000000'
+    ),
+    model_name AS (
+        SELECT
+            block_number AS model_block_number,
+            tx_hash AS model_tx_hash
+        FROM
+            {{ model }}
+    ),
+    FINAL AS (
+        SELECT
+            base_block_number,
+            base_tx_hash,
+            model_block_number,
+            model_tx_hash
+        FROM
+            txs_base
+            LEFT JOIN model_name
+            ON base_block_number = model_block_number
+            AND base_tx_hash = model_tx_hash
+        WHERE
+            model_tx_hash IS NULL
+            OR model_block_number IS NULL
+    )
+SELECT
+    *
+FROM
+    FINAL
+WHERE
+    (
+        SELECT
+            COUNT(*) >= {{ threshold }}
+        FROM
+            FINAL
     )
 {% endmacro %}
