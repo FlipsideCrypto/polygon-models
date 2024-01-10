4182e6b418f7bc597fbc20e711007eb3cf4ad6b2
mattromano <mattromano88@gmail.com>
2024-01-09 16:25:22 -0800

    add poly swaps

--- models/silver/dex/hashflow/silver_dex__hashflow_v3_pools.sql
+++ models/silver/dex/hashflow/silver_dex__hashflow_v3_pools.sql
@@ -5,22 +5,28 @@
     tags = ['curated']
 ) }}
 
-WITH contract_deployments AS (
+WITH logs_pull AS (
 
     SELECT
         tx_hash,
         block_number,
         block_timestamp,
-        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
-        origin_from_address AS deployer_address,
-        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        DATA,
+        contract_address,
+        origin_from_address,
         _log_id,
         _inserted_timestamp
     FROM
         {{ ref('silver__logs') }}
     WHERE
-        contract_address = '0xde828fdc3f497f16416d1bb645261c7c6a62dab5'
-        AND topics [0] :: STRING = '0xdbd2a1ea6808362e6adbec4db4969cbc11e3b0b28fb6c74cb342defaaf1daada'
+        (
+            contract_address = LOWER('0xdE828fdc3F497F16416D1bB645261C7C6a62DAb5')
+            AND topics [0] :: STRING = '0xdbd2a1ea6808362e6adbec4db4969cbc11e3b0b28fb6c74cb342defaaf1daada'
+        )
+        OR 
+        (
+            tx_hash = '0xfaa849569efd415d8859217d2ec907ab531d932d625d8c9feeb48f4829b9fc34'
+        )
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -30,16 +36,51 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 {% endif %}
+),
+contract_deployments AS (
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        origin_from_address AS deployer_address,
+        C.token_name AS pool_name,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        l._log_id,
+        l._inserted_timestamp
+    FROM
+        logs_pull l
+        LEFT JOIN {{ ref('silver__contracts') }} C
+        ON pool_address = c.contract_address
+    WHERE
+        l.contract_address = '0xde828fdc3f497f16416d1bb645261c7c6a62dab5'
 )
 SELECT
     tx_hash,
     block_number,
     block_timestamp,
+    origin_from_address AS deployer_address,
+    'HashflowRouter' AS pool_name,
+    contract_address AS pool_address,
+    _log_id,
+    _inserted_timestamp
+FROM
+    logs_pull
+WHERE
+    tx_hash = '0xfaa849569efd415d8859217d2ec907ab531d932d625d8c9feeb48f4829b9fc34'
+    AND
+    pool_address <> '0x0000000000000000000000000000000000001010'
+UNION ALL
+SELECT
+    tx_hash,
+    block_number,
+    block_timestamp,
     deployer_address,
+    pool_name,
     pool_address,
     _log_id,
     _inserted_timestamp
 FROM
-    contract_deployments qualify(ROW_NUMBER() over (PARTITION BY pool_address
+    contract_deployments qualify(ROW_NUMBER() over(PARTITION BY pool_address
 ORDER BY
     _inserted_timestamp DESC)) = 1
--- models/silver/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
+++ models/silver/dex/hashflow/silver_dex__hashflow_v3_swaps.sql
@@ -9,7 +9,8 @@
 WITH pools AS (
 
     SELECT
-        pool_address
+        pool_address,
+        pool_name
     FROM
         {{ ref('silver_dex__hashflow_v3_pools') }}
 ),
@@ -24,6 +25,7 @@ swaps AS (
         l.event_index,
         contract_address,
         regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        p.pool_name,
         CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS trader_address,
         CONCAT('0x', SUBSTR(segmented_data [1] :: STRING, 25, 40)) AS effective_trader_address,
         CONCAT(
@@ -60,6 +62,75 @@ AND _inserted_timestamp >= (
         {{ this }}
 )
 {% endif %}
+),
+router_swaps AS (
+    SELECT
+        l.block_number,
+        l.block_timestamp,
+        l.tx_hash,
+        l.origin_function_signature,
+        l.origin_from_address,
+        l.origin_to_address,
+        'XChainTrade' AS event_name,
+        l.event_index,
+        contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        'HashflowRouter' AS pool_name,
+        utils.udf_hex_to_int(
+            segmented_data [0] :: STRING
+        ) AS chain_id,
+        CASE
+            --ID 20 = Solana XChainTrade, address is decoded to Solana format
+            WHEN chain_id = '20' THEN utils.udf_hex_to_base58(
+                CONCAT(
+                    '0x',
+                    segmented_data [3] :: STRING
+                )
+            )
+            ELSE CONCAT('0x', SUBSTR(segmented_data [3] :: STRING, 27, 40))
+        END AS tx_to,
+        --The trader wallet address that will swap with the contract. This can be a proxy contract
+        CONCAT('0x', SUBSTR(segmented_data [2] :: STRING, 25, 40)) AS trader_address,
+        --The wallet address of the actual trader
+        segmented_data [4] :: STRING AS txid,
+        CONCAT('0x', SUBSTR(segmented_data [5] :: STRING, 25, 40)) AS tokenIn,
+        segmented_data [6] :: STRING AS raw_token_out,
+        CASE
+            WHEN chain_id = '20' THEN utils.udf_hex_to_base58(
+                CONCAT(
+                    '0x',
+                    segmented_data [6] :: STRING
+                )
+            )
+            ELSE CONCAT('0x', SUBSTR(segmented_data [6] :: STRING, 27, 40))
+        END AS tokenOut,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [7] :: STRING
+            )
+        ) AS amountIn,
+        TRY_TO_NUMBER(
+            utils.udf_hex_to_int(
+                segmented_data [8] :: STRING
+            )
+        ) AS amountOut,
+        l._log_id,
+        l._inserted_timestamp
+    FROM
+        {{ ref('silver__logs') }}
+        l
+    WHERE
+        l.topics [0] :: STRING = '0x3f72b2a38919490277652bb34955c871b20e23068c243319c9fa5e27963d9e12' --Xchaintrade
+        AND origin_to_address = LOWER('0x55084eE0fEf03f14a305cd24286359A35D735151') --router
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
 )
 SELECT
     block_number,
@@ -70,6 +141,7 @@ SELECT
     origin_to_address,
     event_index,
     contract_address,
+    pool_name,
     effective_trader_address AS sender,
     trader_address AS tx_to,
     txid,
@@ -89,3 +161,33 @@ SELECT
     _inserted_timestamp
 FROM
     swaps
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    event_index,
+    contract_address,
+    pool_name,
+    trader_address AS sender,
+    tx_to,
+    txid,
+    CASE
+        WHEN tokenIn = '0x0000000000000000000000000000000000000000' THEN '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ELSE tokenIn
+    END AS token_in,
+    CASE
+        WHEN tokenOut = '0x0000000000000000000000000000000000000000' THEN '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
+        ELSE tokenOut
+    END AS token_out,
+    amountIn AS amount_in_unadj,
+    amountOut AS amount_out_unadj,
+    event_name,
+    'hashflow-v3' AS platform,
+    _log_id,
+    _inserted_timestamp
+FROM
+    router_swaps
