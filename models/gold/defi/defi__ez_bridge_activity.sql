4cb5986a1bf62d7a42847d9932f3be23a5cb0d4c
drethereum <71602799+drethereum@users.noreply.github.com>
2024-01-11 13:00:47 -0500

    AN-3841/bridge-curation (#328)
    * updated contracts
    
    * token symbol
    
    * all bridge filter
    
    * wormhole seed
    
    * filter

---
+++ models/gold/defi/defi__ez_bridge_activity.sql
@@ -0,0 +1,55 @@
+{{ config(
+    materialized = 'view',
+    persist_docs ={ "relation": true,
+    "columns": true },
+    meta ={ 
+        'database_tags':{
+            'table':{
+                'PROTOCOL': 'ACROSS, ALLBRIDGE, AXELAR, CELER, CBRIDGE, HOP, MESON, MULTICHAIN, STARGATE, SYMBIOSIS, SYNAPSE, WORMHOLE',
+                'PURPOSE': 'BRIDGE'
+        } } }
+) }}
+
+SELECT
+    block_number,
+    block_timestamp,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_hash,
+    event_index,
+    bridge_address,
+    event_name,
+    platform,
+    sender,
+    receiver,
+    destination_chain_receiver,
+    COALESCE(
+        standard_destination_chain,
+        b.destination_chain
+    ) AS destination_chain,
+    destination_chain_id,
+    token_address,
+    token_symbol,
+    amount_unadj,
+    amount,
+    amount_usd,
+    COALESCE (
+        complete_bridge_activity_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['_id']
+        ) }}
+    ) AS ez_bridge_activity_id,
+    COALESCE(
+        inserted_timestamp,
+        '2000-01-01'
+    ) AS inserted_timestamp,
+    COALESCE(
+        modified_timestamp,
+        '2000-01-01'
+    ) AS modified_timestamp
+FROM
+    {{ ref('silver_bridge__complete_bridge_activity') }}
+    b
+    LEFT JOIN {{ ref('silver_bridge__standard_dst_chain_seed') }} C
+    ON b.destination_chain = C.destination_chain
---
+++ models/silver/defi/bridge/synapse/silver_bridge__synapse_tokenbridgeandswap.sql
@@ -0,0 +1,93 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['curated','reorg']
+) }}
+
+WITH base_evt AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        origin_function_signature,
+        origin_from_address,
+        origin_to_address,
+        contract_address,
+        'synapse' AS NAME,
+        event_index,
+        topics [0] :: STRING AS topic_0,
+        event_name,
+        TRY_TO_NUMBER(
+            decoded_flat :"amount" :: STRING
+        ) AS amount,
+        TRY_TO_NUMBER(
+            decoded_flat :"chainId" :: STRING
+        ) AS chainId,
+        TRY_TO_TIMESTAMP(
+            decoded_flat :"deadline" :: STRING
+        ) AS deadline,
+        TRY_TO_NUMBER(
+            decoded_flat :"minDy" :: STRING
+        ) AS minDy,
+        decoded_flat :"to" :: STRING AS to_address,
+        decoded_flat :"token" :: STRING AS token,
+        TRY_TO_NUMBER(
+            decoded_flat :"tokenIndexFrom" :: STRING
+        ) AS tokenIndexFrom,
+        TRY_TO_NUMBER(
+            decoded_flat :"tokenIndexTo" :: STRING
+        ) AS tokenIndexTo,
+        decoded_flat,
+        event_removed,
+        tx_status,
+        _log_id,
+        _inserted_timestamp
+    FROM
+        {{ ref('silver__decoded_logs') }}
+    WHERE
+        topics [0] :: STRING = '0x91f25e9be0134ec851830e0e76dc71e06f9dade75a9b84e9524071dbbc319425'
+        AND contract_address IN (
+            '0x8f5bbb2bb8c2ee94639e55d5f41de9b4839c1280',
+            '0x0efc29e196da2e81afe96edd041bedcdf9e74893'
+        )
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    tx_hash,
+    event_index,
+    topic_0,
+    event_name,
+    event_removed,
+    tx_status,
+    contract_address AS bridge_address,
+    NAME AS platform,
+    origin_from_address AS sender,
+    to_address AS receiver,
+    receiver AS destination_chain_receiver,
+    amount,
+    chainId AS destination_chain_id,
+    token AS token_address,
+    deadline,
+    minDy AS min_dy,
+    tokenIndexFrom AS token_index_from,
+    tokenIndexTo AS token_index_to,
+    _log_id,
+    _inserted_timestamp
+FROM
+    base_evt
