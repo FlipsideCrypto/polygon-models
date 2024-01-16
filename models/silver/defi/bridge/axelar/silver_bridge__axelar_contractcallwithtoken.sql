c8b96b7f1e48d628091cc63c3d3aa1eda1d17878
drethereum <71602799+drethereum@users.noreply.github.com>
2024-01-16 16:15:46 -0500

    axelar transfer events and var for reload (#331)

--- models/silver/defi/bridge/axelar/silver_bridge__axelar_contractcallwithtoken.sql
+++ models/silver/defi/bridge/axelar/silver_bridge__axelar_contractcallwithtoken.sql
@@ -111,7 +111,10 @@ transfers AS (
         {{ ref('silver__transfers') }}
     WHERE
         from_address = '0xce16f69375520ab01377ce7b88f5ba8c48f8d666'
-        AND to_address = '0x6f015f16de9fc8791b234ef68d486d2bf203fba8'
+        AND to_address IN (
+            '0x6f015f16de9fc8791b234ef68d486d2bf203fba8',
+            '0x0000000000000000000000000000000000000000'
+        )
 
 {% if is_incremental() %}
 AND _inserted_timestamp >= (
@@ -137,7 +140,7 @@ FINAL AS (
         b.tx_status,
         b.contract_address AS bridge_address,
         b.name AS platform,
-        b.sender,
+        b.origin_from_address AS sender,
         CASE
             WHEN b.recipient = '0x0000000000000000000000000000000000000000' THEN refundAddress
             ELSE b.recipient
