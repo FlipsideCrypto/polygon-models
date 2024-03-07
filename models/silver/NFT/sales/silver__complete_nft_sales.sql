c3e474a5534de80e3d68f8ecaae0f48b89111acb
sam <sam@flipsidecrypto.com>
2024-03-07 17:20:16 +0800

    aggregator logic

--- models/silver/NFT/sales/silver__complete_nft_sales.sql
+++ models/silver/NFT/sales/silver__complete_nft_sales.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized = 'incremental',
     incremental_strategy = 'delete+insert',
-    unique_key = ['block_number','platform_name','platform_exchange_version'],
+    unique_key = ['block_number','platform_exchange_version'],
     cluster_by = ['block_timestamp::DATE'],
     tags = ['curated','reorg', 'heal']
 ) }}
@@ -271,6 +271,21 @@ matic_price AS (
     WHERE
         token_address = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270'
 ),
+contracts_decimal AS (
+    SELECT
+        contract_address AS address_contracts,
+        token_symbol AS symbol_contracts,
+        token_decimals AS decimals_contracts
+    FROM
+        {{ ref('silver__contracts') }}
+    WHERE
+        contract_address IN (
+            SELECT
+                currency_address
+            FROM
+                nft_base_models
+        )
+),
 final_base AS (
     SELECT
         block_number,
@@ -279,12 +294,7 @@ final_base AS (
         event_index,
         event_type,
         platform_address,
-        CASE
-            WHEN origin_to_address IN (
-                '0x5e06c349a4a1b8dde8da31e0f167d1cb1d99967c'
-            ) THEN 'dew'
-            ELSE platform_name
-        END AS platform_name,
+        platform_name,
         platform_exchange_version,
         --credits to hildobby and 0xRob for reservoir calldata logic https://github.com/duneanalytics/spellbook/blob/main/models/nft/ethereum/nft_ethereum_aggregators_markers.sql
         CASE
@@ -316,20 +326,6 @@ final_base AS (
             )
         ) AS marketplace_decoded,
         CASE
-            WHEN RIGHT(
-                input_data,
-                8
-            ) = '72db8c0b'
-            AND block_timestamp :: DATE <= '2023-11-01' THEN 'Gem'
-            WHEN RIGHT(
-                input_data,
-                8
-            ) = '72db8c0b'
-            AND block_timestamp :: DATE >= '2023-11-02' THEN 'OpenSea Pro'
-            WHEN RIGHT(
-                input_data,
-                15
-            ) = '9616c6c64617461' THEN 'Rarible'
             WHEN marketplace_decoded IS NOT NULL THEN marketplace_decoded
             ELSE NULL
         END AS aggregator_name,
@@ -338,10 +334,10 @@ final_base AS (
         nft_address,
         erc1155_value,
         tokenId,
-        CASE
-            WHEN currency_address = 'MATIC' THEN 'MATIC'
-            ELSE p.symbol
-        END AS currency_symbol,
+        COALESCE(
+            p.symbol,
+            symbol_contracts
+        ) AS currency_symbol,
         currency_address,
         total_price_raw,
         total_fees_raw,
@@ -355,12 +351,21 @@ final_base AS (
                 10,
                 18
             )
-            ELSE COALESCE (total_price_raw / pow(10, decimals), total_price_raw)
+            ELSE COALESCE (
+                total_price_raw / pow(10, COALESCE(p.decimals, decimals_contracts)),
+                total_price_raw
+            )
         END AS price,
         IFF(
-            decimals IS NULL,
+            COALESCE(
+                p.decimals,
+                decimals_contracts
+            ) IS NULL,
             0,
-            price * hourly_prices
+            price * COALESCE(
+                hourly_prices,
+                0
+            )
         ) AS price_usd,
         CASE
             WHEN currency_address IN (
@@ -370,8 +375,22 @@ final_base AS (
                 10,
                 18
             )
-            ELSE COALESCE (total_fees_raw / pow(10, decimals), total_fees_raw)
+            ELSE COALESCE (
+                total_fees_raw / pow(10, COALESCE(p.decimals, decimals_contracts)),
+                total_fees_raw
+            )
         END AS total_fees,
+        IFF(
+            COALESCE(
+                p.decimals,
+                decimals_contracts
+            ) IS NULL,
+            0,
+            total_fees * COALESCE(
+                hourly_prices,
+                0
+            )
+        ) AS total_fees_usd,
         CASE
             WHEN currency_address IN (
                 'MATIC',
@@ -380,8 +399,22 @@ final_base AS (
                 10,
                 18
             )
-            ELSE COALESCE (platform_fee_raw / pow(10, decimals), platform_fee_raw)
+            ELSE COALESCE (
+                platform_fee_raw / pow(10, COALESCE(p.decimals, decimals_contracts)),
+                platform_fee_raw
+            )
         END AS platform_fee,
+        IFF(
+            COALESCE(
+                p.decimals,
+                decimals_contracts
+            ) IS NULL,
+            0,
+            platform_fee * COALESCE(
+                hourly_prices,
+                0
+            )
+        ) AS platform_fee_usd,
         CASE
             WHEN currency_address IN (
                 'MATIC',
@@ -390,22 +423,21 @@ final_base AS (
                 10,
                 18
             )
-            ELSE COALESCE (creator_fee_raw / pow(10, decimals), creator_fee_raw)
+            ELSE COALESCE (
+                creator_fee_raw / pow(10, COALESCE(p.decimals, decimals_contracts)),
+                creator_fee_raw
+            )
         END AS creator_fee,
         IFF(
-            decimals IS NULL,
+            COALESCE(
+                p.decimals,
+                decimals_contracts
+            ) IS NULL,
             0,
-            total_fees * hourly_prices
-        ) AS total_fees_usd,
-        IFF(
-            decimals IS NULL,
-            0,
-            platform_fee * hourly_prices
-        ) AS platform_fee_usd,
-        IFF(
-            decimals IS NULL,
-            0,
-            creator_fee * hourly_prices
+            creator_fee * COALESCE(
+                hourly_prices,
+                0
+            )
         ) AS creator_fee_usd,
         tx_fee,
         tx_fee * matic_hourly_price AS tx_fee_usd,
@@ -429,11 +461,13 @@ final_base AS (
             'hour',
             b.block_timestamp
         ) = m.hour
-)
+        LEFT JOIN contracts_decimal C
+        ON b.currency_address = C.address_contracts
+),
 
 {% if is_incremental() and var(
     'HEAL_MODEL'
-) %},
+) %}
 heal_model AS (
     SELECT
         block_number,
@@ -442,11 +476,17 @@ heal_model AS (
         event_index,
         event_type,
         platform_address,
-        platform_name,
+        COALESCE(
+            a2.aggregator,
+            platform_name
+        ) AS platform_name,
         platform_exchange_version,
         calldata_hash,
         marketplace_decoded,
-        aggregator_name,
+        COALESCE(
+            aggregator_name,
+            A.aggregator
+        ) AS aggregator_name,
         seller_address,
         buyer_address,
         nft_address,
@@ -481,88 +521,184 @@ heal_model AS (
         t
         LEFT JOIN {{ ref('silver__contracts') }} C
         ON t.nft_address = C.contract_address
+        LEFT JOIN {{ ref('silver__aggregator_list') }} A
+        ON RIGHT(
+            t.input_data,
+            8
+        ) = A.aggregator_identifier
+        AND aggregator_type = 'calldata'
+        LEFT JOIN {{ ref('silver__aggregator_list') }}
+        a2
+        ON t.origin_to_address = a2.aggregator_identifier
+        AND a2.aggregator_type = 'router'
     WHERE
-        t.block_number IN (
-            SELECT
-                DISTINCT t1.block_number AS block_number
-            FROM
-                {{ this }}
-                t1
-            WHERE
-                t1.project_name IS NULL
-                AND _inserted_timestamp < (
+        (
+            t.block_number IN (
+                SELECT
+                    DISTINCT t1.block_number AS block_number
+                FROM
+                    {{ this }}
+                    t1
+                WHERE
+                    t1.project_name IS NULL
+                    AND _inserted_timestamp < (
+                        SELECT
+                            MAX(
+                                _inserted_timestamp
+                            ) - INTERVAL '36 hours'
+                        FROM
+                            {{ this }}
+                    )
+                    AND EXISTS (
+                        SELECT
+                            1
+                        FROM
+                            {{ ref('silver__contracts') }} C
+                        WHERE
+                            C._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                            AND C.token_name IS NOT NULL
+                            AND C.contract_address = t1.nft_address)
+                    )
+            )
+            OR (
+                t.block_number IN (
                     SELECT
-                        MAX(
-                            _inserted_timestamp
-                        ) - INTERVAL '36 hours'
+                        DISTINCT t1.block_number AS block_number
                     FROM
                         {{ this }}
-                )
-                AND EXISTS (
-                    SELECT
-                        1
-                    FROM
-                        {{ ref('silver__contracts') }} C
+                        t1
                     WHERE
-                        C._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
-                        AND C.token_name IS NOT NULL
-                        AND C.contract_address = t1.nft_address)
+                        t1.aggregator_name IS NULL
+                        AND _inserted_timestamp < (
+                            SELECT
+                                MAX(
+                                    _inserted_timestamp
+                                ) - INTERVAL '36 hours'
+                            FROM
+                                {{ this }}
+                        )
+                        AND EXISTS (
+                            SELECT
+                                1
+                            FROM
+                                {{ ref('silver__aggregator_list') }} A
+                            WHERE
+                                A._inserted_timestamp > DATEADD('DAY', -2, SYSDATE())
+                                AND A.aggregator_type = 'calldata'
+                                AND RIGHT(
+                                    t1.input_data,
+                                    8
+                                ) = A.aggregator_identifier
+                        )
                 )
-        )
-    {% endif %}
-    SELECT
-        block_number,
-        block_timestamp,
-        tx_hash,
-        event_index,
-        event_type,
-        platform_address,
-        platform_name,
-        platform_exchange_version,
-        calldata_hash,
-        marketplace_decoded,
-        aggregator_name,
-        seller_address,
-        buyer_address,
-        nft_address,
-        C.token_name AS project_name,
-        erc1155_value,
-        tokenId,
-        currency_symbol,
-        currency_address,
-        total_price_raw,
-        total_fees_raw,
-        platform_fee_raw,
-        creator_fee_raw,
-        price,
-        price_usd,
-        total_fees,
-        total_fees_usd,
-        platform_fee,
-        platform_fee_usd,
-        creator_fee,
-        creator_fee_usd,
-        tx_fee,
-        tx_fee_usd,
-        origin_from_address,
-        origin_to_address,
-        origin_function_signature,
-        nft_log_id,
-        input_data,
-        _log_id,
-        b._inserted_timestamp,
-        {{ dbt_utils.generate_surrogate_key(
-            ['tx_hash', 'event_index', 'nft_address','tokenId','platform_exchange_version']
-        ) }} AS complete_nft_sales_id,
-        SYSDATE() AS inserted_timestamp,
-        SYSDATE() AS modified_timestamp,
-        '{{ invocation_id }}' AS _invocation_id
-    FROM
-        final_base b
-        LEFT JOIN {{ ref('silver__contracts') }} C
-        ON b.nft_address = C.contract_address qualify(ROW_NUMBER() over(PARTITION BY nft_log_id
-    ORDER BY
-        b._inserted_timestamp DESC)) = 1
+            )
+            OR (
+                t.block_number IN (
+                    SELECT
+                        DISTINCT t1.block_number AS block_number
+                    FROM
+                        {{ this }}
+                        t1
+                    WHERE
+                        t1.origin_to_address IN (
+                            SELECT
+                                aggregator_identifier
+                            FROM
+                                {{ ref('silver__aggregator_list') }}
+                            WHERE
+                                aggregator_type = 'router'
+                                AND _inserted_timestamp >= DATEADD('DAY', -2, SYSDATE()))
+                                AND _inserted_timestamp < (
+                                    SELECT
+                                        MAX(
+                                            _inserted_timestamp
+                                        ) - INTERVAL '36 hours'
+                                    FROM
+                                        {{ this }}
+                                )
+                                AND EXISTS (
+                                    SELECT
+                                        1
+                                    FROM
+                                        {{ ref('silver__aggregator_list') }}
+                                        a2
+                                    WHERE
+                                        a2._inserted_timestamp > DATEADD('DAY', -2, SYSDATE())
+                                        AND t1.origin_to_address = a2.aggregator_identifier
+                                        AND a2.aggregator_type = 'router')
+                                )
+                        )
+                ),
+                {% endif %}
+
+                combined AS (
+                    SELECT
+                        block_number,
+                        block_timestamp,
+                        tx_hash,
+                        event_index,
+                        event_type,
+                        platform_address,
+                        COALESCE(
+                            a2.aggregator,
+                            platform_name
+                        ) AS platform_name,
+                        platform_exchange_version,
+                        calldata_hash,
+                        marketplace_decoded,
+                        COALESCE(
+                            aggregator_name,
+                            A.aggregator
+                        ) AS aggregator_name,
+                        seller_address,
+                        buyer_address,
+                        nft_address,
+                        C.token_name AS project_name,
+                        erc1155_value,
+                        tokenId,
+                        currency_symbol,
+                        currency_address,
+                        total_price_raw,
+                        total_fees_raw,
+                        platform_fee_raw,
+                        creator_fee_raw,
+                        price,
+                        price_usd,
+                        total_fees,
+                        total_fees_usd,
+                        platform_fee,
+                        platform_fee_usd,
+                        creator_fee,
+                        creator_fee_usd,
+                        tx_fee,
+                        tx_fee_usd,
+                        origin_from_address,
+                        origin_to_address,
+                        origin_function_signature,
+                        nft_log_id,
+                        input_data,
+                        _log_id,
+                        b._inserted_timestamp,
+                        {{ dbt_utils.generate_surrogate_key(
+                            ['tx_hash', 'event_index', 'nft_address','tokenId','platform_exchange_version']
+                        ) }} AS complete_nft_sales_id,
+                        SYSDATE() AS inserted_timestamp,
+                        SYSDATE() AS modified_timestamp,
+                        '{{ invocation_id }}' AS _invocation_id
+                    FROM
+                        final_base b
+                        LEFT JOIN {{ ref('silver__contracts') }} C
+                        ON b.nft_address = C.contract_address
+                        LEFT JOIN {{ ref('silver__aggregator_list') }} A
+                        ON RIGHT(
+                            b.input_data,
+                            8
+                        ) = A.aggregator_identifier
+                        AND A.aggregator_type = 'calldata'
+                        LEFT JOIN {{ ref('silver__aggregator_list') }}
+                        a2
+                        ON b.origin_to_address = a2.aggregator_identifier
+                        AND a2.aggregator_type = 'router'
 
 {% if is_incremental() and var(
     'HEAL_MODEL'
@@ -618,3 +754,53 @@ SELECT
 FROM
     heal_model
 {% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    event_index,
+    event_type,
+    platform_address,
+    platform_name,
+    platform_exchange_version,
+    calldata_hash,
+    marketplace_decoded,
+    aggregator_name,
+    seller_address,
+    buyer_address,
+    nft_address,
+    project_name,
+    erc1155_value,
+    tokenId,
+    currency_symbol,
+    currency_address,
+    total_price_raw,
+    total_fees_raw,
+    platform_fee_raw,
+    creator_fee_raw,
+    price,
+    price_usd,
+    total_fees,
+    total_fees_usd,
+    platform_fee,
+    platform_fee_usd,
+    creator_fee,
+    creator_fee_usd,
+    tx_fee,
+    tx_fee_usd,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    nft_log_id,
+    input_data,
+    _log_id,
+    _inserted_timestamp,
+    complete_nft_sales_id,
+    inserted_timestamp,
+    modified_timestamp,
+    _invocation_id
+FROM
+    combined qualify (ROW_NUMBER() over(PARTITION BY nft_log_id
+ORDER BY
+    _inserted_timestamp DESC)) = 1
