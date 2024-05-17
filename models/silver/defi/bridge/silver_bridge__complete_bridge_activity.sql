7cc35bddbdbbce00b4990e762363bb69b07114c3
drethereum <trevor.wenokur@gmail.com>
2024-05-16 21:53:13 -0600

    bridge heal

--- models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
+++ models/silver/defi/bridge/silver_bridge__complete_bridge_activity.sql
@@ -45,7 +45,6 @@ WHERE
 {% endif %}
 ),
 across_v3 AS (
-
     SELECT
         block_number,
         block_timestamp,
@@ -576,7 +575,7 @@ all_protocols AS (
         celer_cbridge
     UNION ALL
     SELECT
-    *
+        *
     FROM
         dln_debridge
     UNION ALL
@@ -706,6 +705,198 @@ FINAL AS (
         ) = LOWER(
             b.destination_chain
         )
+),
+
+{% if is_incremental() and var(
+    'HEAL_MODEL'
+) %}
+heal_model AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        event_index,
+        bridge_address,
+        event_name,
+        platform,
+        version,
+        sender,
+        receiver,
+        destination_chain_receiver,
+        CASE
+            WHEN platform IN (
+                'stargate',
+                'wormhole',
+                'meson'
+            ) THEN destination_chain_id :: STRING
+            WHEN d.chain_id IS NULL THEN destination_chain_id :: STRING
+            ELSE d.chain_id :: STRING
+        END AS destination_chain_id,
+        CASE
+            WHEN platform IN (
+                'stargate',
+                'wormhole',
+                'meson'
+            ) THEN LOWER(destination_chain)
+            WHEN d.chain IS NULL THEN LOWER(destination_chain)
+            ELSE LOWER(
+                d.chain
+            )
+        END AS destination_chain,
+        t0.token_address,
+        CASE
+            WHEN platform = 'axelar' THEN COALESCE(
+                C.token_symbol,
+                t0.token_symbol
+            )
+            ELSE C.token_symbol
+        END AS token_symbol,
+        C.token_decimals AS token_decimals,
+        amount_unadj,
+        CASE
+            WHEN C.token_decimals IS NOT NULL THEN (amount_unadj / pow(10, C.token_decimals))
+            ELSE amount_unadj
+        END AS amount,
+        CASE
+            WHEN C.token_decimals IS NOT NULL THEN ROUND(
+                amount * p.price,
+                2
+            )
+            ELSE NULL
+        END AS amount_usd_unadj,
+        _id,
+        t0._inserted_timestamp
+    FROM
+        {{ this }}
+        t0
+        LEFT JOIN {{ ref('silver__contracts') }} C
+        ON t0.token_address = C.contract_address
+        LEFT JOIN {{ ref('price__ez_prices_hourly') }}
+        p
+        ON t0.token_address = p.token_address
+        AND DATE_TRUNC(
+            'hour',
+            block_timestamp
+        ) = p.hour
+        LEFT JOIN {{ source(
+            'external_gold_defillama',
+            'dim_chains'
+        ) }}
+        d
+        ON d.chain_id :: STRING = t0.destination_chain_id :: STRING
+        OR LOWER(
+            d.chain
+        ) = LOWER(
+            t0.destination_chain
+        )
+    WHERE
+        CONCAT(
+            t0.block_number,
+            '-',
+            t0.platform,
+            '-',
+            t0.version
+        ) IN (
+            SELECT
+                CONCAT(
+                    t1.block_number,
+                    '-',
+                    t1.platform,
+                    '-',
+                    t1.version
+                )
+            FROM
+                {{ this }}
+                t1
+            WHERE
+                t1.token_decimals IS NULL
+                AND t1._inserted_timestamp < (
+                    SELECT
+                        MAX(
+                            _inserted_timestamp
+                        ) - INTERVAL '{{ var(' lookback ', ' 4 hours ') }}'
+                    FROM
+                        {{ this }}
+                )
+                AND EXISTS (
+                    SELECT
+                        1
+                    FROM
+                        {{ ref('silver__contracts') }} C
+                    WHERE
+                        C._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                        AND C.token_decimals IS NOT NULL
+                        AND C.contract_address = t1.token_address)
+                    GROUP BY
+                        1
+                )
+                OR CONCAT(
+                    t0.block_number,
+                    '-',
+                    t0.platform,
+                    '-',
+                    t0.version
+                ) IN (
+                    SELECT
+                        CONCAT(
+                            t2.block_number,
+                            '-',
+                            t2.platform,
+                            '-',
+                            t2.version
+                        )
+                    FROM
+                        {{ this }}
+                        t2
+                    WHERE
+                        t2.amount_usd IS NULL
+                        AND t2._inserted_timestamp < (
+                            SELECT
+                                MAX(
+                                    _inserted_timestamp
+                                ) - INTERVAL '{{ var(' lookback ', ' 4 hours ') }}'
+                            FROM
+                                {{ this }}
+                        )
+                        AND EXISTS (
+                            SELECT
+                                1
+                            FROM
+                                {{ ref('silver__complete_token_prices') }}
+                                p
+                            WHERE
+                                p._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                                AND p.price IS NOT NULL
+                                AND p.token_address = t2.token_address
+                                AND p.hour = DATE_TRUNC(
+                                    'hour',
+                                    t2.block_timestamp
+                                )
+                        )
+                    GROUP BY
+                        1
+                )
+        ),
+    {% endif %}
+
+    FINAL AS (
+        SELECT
+            *
+        FROM
+            complete_bridge_activity
+
+{% if is_incremental() and var(
+    'HEAL_MODEL'
+) %}
+UNION ALL
+SELECT
+    *
+FROM
+    heal_model
+{% endif %}
 )
 SELECT
     block_number,
@@ -729,21 +920,18 @@ SELECT
     token_decimals,
     amount_unadj,
     amount,
-    CASE
-        WHEN amount_usd_unadj < 1e+15 THEN amount_usd_unadj
-        ELSE NULL
-    END AS amount_usd,
+    amount_usd,
     _id,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(
-    ['_id']
+        ['_id']
     ) }} AS complete_bridge_activity_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
 FROM
-    FINAL 
-WHERE destination_chain <> 'polygon' 
-qualify (ROW_NUMBER() over (PARTITION BY _id
+    FINAL
+WHERE
+    destination_chain <> 'optimism' qualify (ROW_NUMBER() over (PARTITION BY _id
 ORDER BY
     _inserted_timestamp DESC)) = 1
