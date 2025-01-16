02b384f54d8f6668a447c38ac0569f8a6818e5b1
drethereum <trevor.wenokur@gmail.com>
2025-01-16 14:18:16 -0700

    coalesce transaction index

--- models/streamline/bronze/core/core/bronze__receipts_fr.sql
+++ models/streamline/bronze/core/core/bronze__receipts_fr.sql
@@ -19,7 +19,14 @@ UNION ALL
 SELECT
     _partition_by_block_id AS partition_key,
     block_number,
-    VALUE :"array_index" :: INT AS array_index,
+    COALESCE(
+          VALUE :"array_index" :: INT,
+          TRY_TO_NUMBER(
+              utils.udf_hex_to_int(
+                  VALUE :"data" :"transactionIndex" :: STRING
+              )
+          )
+      ) AS array_index,
     VALUE,
     DATA,
     metadata,
