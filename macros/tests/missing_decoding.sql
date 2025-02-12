89163debe4401da6ecdbc1e9af879646f5a56252
Matt Romano <42412983+mattromano@users.noreply.github.com>
2025-02-12 12:54:22 -0800

    fix ref (#422)

--- macros/tests/missing_decoding.sql
+++ macros/tests/missing_decoding.sql
@@ -1,7 +1,7 @@
 {% test missing_decoded_logs(model) %}
 SELECT
     l.block_number,
-    l.CONCAT(tx_hash :: STRING, '-', event_index :: STRING) AS _log_id
+    CONCAT(l.tx_hash :: STRING, '-', l.event_index :: STRING) AS _log_id
 FROM
     {{ ref('core__fact_event_logs') }}
     l
@@ -18,4 +18,5 @@ WHERE
     AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
     AND l.block_timestamp BETWEEN DATEADD('hour', -48, SYSDATE())
     AND DATEADD('hour', -6, SYSDATE())
-    AND d._log_id IS NULL {% endtest %}
+    AND d._log_id IS NULL 
+{% endtest %}
